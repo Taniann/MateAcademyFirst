a75115663fb4e4ebc4be508930224d1878cc4b94
Tania Nebesna <nebesnatania@outlook.com>
2019-02-15 16:07:49 +0200

    jdbc lesson work

---
+++ src/main/java/ua/mateacademy/tania/lesson17/jdbc/ConnectionDBWithoutPool.java
@@ -0,0 +1,31 @@
+package ua.mateacademy.tania.lesson17.jdbc;
+
+import oracle.jdbc.driver.OracleDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Locale;
+
+/**
+ * Created by Tania Nebesna on 12.02.2019.
+ */
+public class ConnectionDBWithoutPool {
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    public static final String USER = "MA_STUDENT";
+    public static final String PASS = "KOLOBOK";
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Locale.setDefault(Locale.ENGLISH);
+            DriverManager.registerDriver(new OracleDriver());
+            connection = DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
